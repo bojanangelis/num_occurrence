3e43eafc0490d4c8a3341b6261dbfd93680d55ef
bojanangelis <bojanangeleski98@gmail.com>
2023-08-24 21:30:35 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int numOccurrence(int n, int c) {
+    if (n == 0) return 0;
+    if (n % 10 == c) return 1 + numOccurrence(n / 10, c);
+    else return numOccurrence(n / 10, c);
+}
+
+int main() {
+    int n, c;
+    scanf("%d %d", &n, &c);
+    printf("%d\n", numOccurrence(n, c));
+    return 0;
+}
