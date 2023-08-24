383b1fe681dad3a40ad79c075023b2bfca1ddd14
bojanangelis <bojanangeleski98@gmail.com>
2023-08-24 21:35:27 +0200

    update

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int numOccurrence(int n, int c) {
     if (n == 0) return 0;
+    if (n % 10 == c && (n / 10) % 10 == c) return 2 + numOccurrence(n / 10, c);
     if (n % 10 == c) return 1 + numOccurrence(n / 10, c);
     else return numOccurrence(n / 10, c);
 }
